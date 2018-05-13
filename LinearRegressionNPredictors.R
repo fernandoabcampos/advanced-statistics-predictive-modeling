324a4347d388d1b657ea5847591c115d7094f466
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-05-13 22:31:56 +0200

    Qualitative and Quantitative linear regression

--- LinearRegressionNPredictors.R
+++ LinearRegressionNPredictors.R
@@ -2,16 +2,43 @@
 df <- read.csv("2016_clean.csv")
 head(df)
 attach(df)
+names(df)
 
-model <- lm(formula =  HS ~ GpC + LE + GC, data = df)
-model
-summary(model)$r.sq
+model.quant <- lm(formula =  HS ~ GpC + LE + GC, data = df)
+model.quant
+summary(model.quant)$r.sq
+summary(model.quant)
+
+?lm
+
+df2 = within(df, Region <- relevel(Region, ref = "Western Europe"))
+str(df)
+str(df2)
+
+model.qualit <- lm(formula =  df2$HS ~ df2$GpC + df2$LE + df2$GC + df2$Region, data = df2)
+coef(model.qualit)
+coef(model.quant)
+
+(Intercept) 
+
+summary(model.quant)
+summary(model.qualit)$r.sq
+summary(model.qualit)
 
 
-ggplot(df, aes(x = , y = `Premature Death`)) + geom_point(aes(color = )) + 
-  xlab("Percentage of population under \n age 65 without health insurance") + 
-  ylab("Years of potential life lost before \n age 75 per 100,000 population") + 
-  ggtitle("(b) Premature Death & Uninsured")
+models <- c("Apartado 1.1", "Apartado 1.2")
+ajuste <- c(summary(model.quant)$r.sq, summary(model.qualit)$r.sq)
+kable(data.frame(variables=models,'Bondad del Ajuste'=ajuste))
+
+library('kableExtra')
+models <- c("Apartado 1.1", "Apartado 1.2")
+ajuste <- c(summary(model.quant)$r.sq, summary(model.qualit)$r.sq)
+dfE <- data.frame(variables=models,'Bondad del Ajuste'=as.vector(ajuste))
+kable(dfE)
+
+
+
+
 
 library(ggplot2)
 par( mfrow=c(1,3))
