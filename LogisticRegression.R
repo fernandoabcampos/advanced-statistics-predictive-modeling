be2e79e2ff59ca9c9638bea91e4115c3450cd96a
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-05-15 23:49:11 +0200

    Discrimination Threshold / Confusion matrix

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -1,7 +1,10 @@
 # Logistic Regression exercises
+library(knitr)
+
 df <- read.csv("2016_clean.csv")
 attach(df)
 df$best <- ifelse(df$HR <=32, TRUE, FALSE)
+detach(df)
 m3 <- glm(best ~ GpC + GC,family = binomial, data = df)
 m3
 summary(m3)
@@ -22,6 +25,34 @@ new.df <- data.frame(GpC = 1.5, GC = 0.35)
 predict(m3, new.df, type = "response")
 
 
+m4 <- glm(best ~ GpC + GC + Freedom,family = binomial, data = df)
+m5 <- glm(best ~ GpC + GC + Region,family = binomial, data = df)
+m6 <- glm(best ~ GpC + GC + Freedom + Region,family = binomial, data = df)
+
+models <- c("m3. Inicial (GpC + GC)", "m4. Inicial + Freedom", "m5. Inicial + Region", "m6. Inicial + Freedom + Region")
+aics <- c(AIC(m3), AIC(m4), AIC(m5), AIC(m6))
+kable(data.frame(Modelos=models,AIC=aics))
+
+summary(m4)
+summary(m5)
+summary(m6)
+
+
+
+contrasts (best)
+cor(df)
+str(df)
+cor(df [,-c(1, 2, 14)])
+plot(GpC)
+
+
+model.probs = predict(m6, type="response")
+
+#1st command creates a vector of N FALSE elements. The 2nd transforms to TRUE all that predicted probability of a country being among 32 best exceeds threshold.
+model.pred = rep(FALSE, nrow(df))
+threshold <- 0.8 # suponiendo un umbral de discriminación del 80%
+model.pred[model.probs >= threshold] = TRUE
+table(model.pred, best)
 
 
 #https://stats.stackexchange.com/questions/52475/how-are-the-p-values-of-the-glm-in-r-calculated/52476
