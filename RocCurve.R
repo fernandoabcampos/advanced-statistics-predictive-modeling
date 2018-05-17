850907b831f629a9e4f17c25d12d24c72a6ecca2
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-05-17 23:14:54 +0200

    ROC Curve

---
+++ RocCurve.R
@@ -0,0 +1,13 @@
+#ROC Curve
+library(pROC)
+df$best <- ifelse(df$HR <=32, 1, 0)
+attach(df)
+roc_obj <- roc(best, model.probsM5)
+auc(roc_obj)
+
+
+roc_df <- data.frame(
+  TPR=rev(roc_obj$sensitivities), 
+  FPR=rev(1 - roc_obj$specificities), 
+  labels=roc_obj$response, 
+  scores=roc_obj$predictor)
