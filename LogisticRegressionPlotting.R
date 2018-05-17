8d95a945dc16bf8dcb91ac3dd5268a91832d1a40
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-05-17 22:50:21 +0200

    Logistic regression

--- LogisticRegressionPlotting.R
+++ LogisticRegressionPlotting.R
@@ -16,12 +16,47 @@ table(model.pred, best)
 
 predict(m6, type="response")
 
-model.pred
-options(scipen=999)
+#options(scipen=999)
 
 new.df <- df
-new.df$predicted = model.probs
+df$predicted = model.probs
 #newdat <- data.frame(hp=seq(min(mtcars$hp), max(mtcars$hp),len=100))
 #newdat$vs = predict(fit, newdata=newdat, type="response")
-plot(best~Country, data=df, col="red4")
-lines(best ~ Country, new.df, col="green4", lwd=2)
+
+detach(df)
+attach(new.df)
+
+with(new.df, plot(HR ~ predicted, lwd = 2))
+threshold
+df.filtered <- new.df[which(new.df$predicted >=0.85),]
+
+
+threshold<-0.85
+df.filtered85 <- new.df[which(new.df$predicted >=0.85),]
+detach(df.filtered)
+attach(df.filtered85)
+par(las=1)
+with(df.filtered, plot(HR ~ predicted, col = ifelse(HR > 32,'red','green'), pch = 19))
+text(HR ~ predicted, labels=paste(HR, "-", Country), cex= 0.6, pos=1, lty=0.1)
+
+par(las=1)
+with(df.filtered, plot(HR ~ predicted, col = ifelse(HR > 32,'red','green'), pch = 19))
+text(HR ~ predicted, labels=paste(HR, "-", Country), cex= 0.6, pos=1, lty=0.1)
+
+
+
+model.probsM5 = predict(m5, type="response")
+model.predM5 = rep(FALSE, nrow(df)) #1st crear un vector de N FALSE elementos.
+model.predM5[model.probsM5 >= threshold] = TRUE # Transformar a TRUE predicciones que superan probabilidad del umbral.
+detach(df.filtered)
+attach(df)
+table(model.predM5, best)
+
+
+df$predictedM5 = model.probsM5
+detach(df)
+df.filteredM5 <- df[which(df$predictedM5 >=0.8),]
+attach(df.filteredM5)
+with(df.filteredM5, plot(HR ~ predictedM5, col = ifelse(HR > 32,'red','green'), pch = 19))
+text(HR ~ predictedM5, labels=paste(HR, "-", Country), cex= 0.4)
+
